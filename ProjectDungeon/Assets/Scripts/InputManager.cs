de2cc9e2343673038e2a5a504a3e2ed06ebdcb0e
marsh189 <marsh189@mail.chapman.edu>
2018-06-11 15:30:29 -0700

    update to movement so collision works and added dodge move basics

---
+++ ProjectDungeon/Assets/Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputManager //used to get inputs
+{
+    public static float MoveHorizontal()
+    {
+        float value = 0.0f;
+        value += Input.GetAxis("Horizontal");
+        value += Input.GetAxis("Joystick_Horizontal");
+        return Mathf.Clamp(value, -1.0f, 1.0f); //makes sure value is not greater than 1 (so player cannot use keyboard and joystick to go faster)
+    }
+
+    public static float MoveVertical()
+    {
+        float value = 0.0f;
+        value += Input.GetAxis("Vertical");
+        value += Input.GetAxis("Joystick_Vertical");
+        return Mathf.Clamp(value, -1.0f, 1.0f);
+    }
+
+    public static bool AttackButton()
+    {
+        return Input.GetButtonDown("Attack");
+    }
+
+    public static bool InteractButton()
+    {
+        return Input.GetButtonDown("Interact");
+    }
+
+    public static bool DodgeButton()
+    {
+        return Input.GetButtonDown("Dodge");
+    }
+
+}
--- ProjectDungeon/Assets/Scripts/PlayerController.cs
+++ ProjectDungeon/Assets/Scripts/PlayerController.cs
@@ -3,15 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float speed;
+
+    public float moveSpeed;
+    public float dodgeSpeed;
+    public bool isDodging;
+    public bool isAttacking;
+    int dodgeTimer;
+
+    private Rigidbody2D rb;
+    private Vector2 direction = new Vector2(); //used for finding the direction the player is facing without rotating player
+    private float xMovement;
+    private float yMovement;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0));
 
-	}
+        if(InputManager.AttackButton()) //check if attacking
+        {
+            isAttacking = true;
+        }
+        if(InputManager.DodgeButton()) //check if dodging
+        {
+            dodgeTimer = 15;
+            isDodging = true;
+        }
+
+
+        if (!isDodging && !isAttacking) 
+        {
+            xMovement = InputManager.MoveHorizontal(); //get horizontal movement
+            yMovement = InputManager.MoveVertical();   //get vertical movement
+            
+            direction = new Vector2(xMovement,yMovement);
+
+            Physics2D.Raycast(transform.position, direction); //make raycast to show facing direction, used for attacking? 
+            Debug.DrawRay(transform.position, direction * moveSpeed, Color.green);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!isDodging && !isAttacking)
+        {
+            rb.velocity = direction.normalized * moveSpeed; //move player
+        }
+        else if(isDodging)
+        {
+            if (dodgeTimer > 0)
+            {
+                rb.velocity = direction.normalized * dodgeSpeed; //dodge
+                dodgeTimer--;
+            }
+            else
+            {
+                isDodging = false; //end dodge
+            }
+        }
+    }
 }
