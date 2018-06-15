75b0db4cc59e819d52f412344f378225ee7e63f3
mbruts101 <bruts101@mail.chapman.edu>
2018-06-15 16:08:11 -0700

    Chasing Enemy

---
+++ ProjectDungeon/Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasePlayer : MonoBehaviour {
+    GameObject Player;
+    bool foundPlayer = false;
+    public float detectionRadius;
+    public Collider2D[] searchForPlayer;
+    public int speed;
+	// Use this for initialization
+	void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (!foundPlayer)
+        {
+            searchForPlayer = Physics2D.OverlapCircleAll(this.gameObject.transform.position, detectionRadius);
+            foreach (Collider2D hit in searchForPlayer)
+            {
+                if (hit.gameObject.tag == "Player")
+                {
+                    StartCoroutine(Chase());
+                    foundPlayer = true;
+                }
+            }
+        }
+    }
+    IEnumerator Chase()
+    {
+        while (true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
+            yield return null;
+        }
+        
+    }
+}
